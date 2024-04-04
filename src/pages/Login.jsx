735ac9c4eb521bf6136dcc2f5a989cfde3ea8b61
redroboto <vince.pleyto@gmail.com>
2024-04-04 16:28:16 +0800

    add functions that allow Login page to capture input data and send it to Avion API to log user in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,9 +8,54 @@ import {
 import { Flex } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import ".././css/Login.css";
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    userLogin(formData["email"], formData["password"]);
+  };
+
+  async function userLogin(email, password) {
+    try {
+      const data = await fetch("http://206.189.91.54/api/v1/auth/sign_in", {
+        method: "POST",
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const response = await data.json();
+      console.log(response);
+      if (response.errors) {
+        throw response.errors.full_messages[0];
+      }
+    } catch (error) {
+      console.log("Error Occurred");
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <div className="login-form-container">
@@ -19,16 +64,22 @@ export default function Login() {
         <FormControl>
           <Flex direction="column" align="center" justify="center" gap="1rem">
             <Input
+              name="email"
               type="email"
+              value={formData["email"]}
+              onChange={handleInputChange}
               placeholder="Enter your email address here"
               width="30vw"
             />
             <Input
+              name="password"
               type="password"
+              value={formData["password"]}
+              onChange={handleInputChange}
               placeholder="Enter your password here"
               width="30vw"
             />
-            <Button type="submit" colorScheme="blue">
+            <Button type="submit" colorScheme="blue" onClick={handleSubmit}>
               Sign-in
             </Button>
           </Flex>
