c95e1eba6250de39954f537fe3f7a03088f22d75
redroboto <vince.pleyto@gmail.com>
2024-04-08 18:26:03 +0800

    add function to Messages.jsx to allow fetching of recent direct messages from Slack API. Add function to Conversation.jsx to enable fetching of direct messages between users from Slack API

--- src/pages/DashboardChildren/Conversation.jsx
+++ src/pages/DashboardChildren/Conversation.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { BASE_URL } from "../../utilities/ConstList";
+import { useState, useEffect } from "react";
+
+async function fetchRecentConversation(headers, user_id) {
+  try {
+    const data = await fetch(
+      `${BASE_URL}/messages?receiver_id=${params.id}&receiver_class=User`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          ...headers,
+        },
+      }
+    );
+
+    const res = await data.json();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default function Conversation() {
   const params = useParams();
+  const headers = JSON.parse(localStorage.getItem("headers") || "{}");
+  const [conversation, setConversation] = useState([]);
+
+  async function getRecentConversation() {
+    const data = await fetchRecentConversation(headers, params.user_id);
+    console.log(data);
+    // setConversation(data.data);
+  }
+
+  useEffect(() => {
+    if (headers && params.user_id) {
+      getRecentConversation();
+    }
+  }, [headers, params.user_id]);
 
   return (
     <>
--- src/pages/DashboardChildren/Messages.jsx
+++ src/pages/DashboardChildren/Messages.jsx
@@ -2,8 +2,45 @@ import { Input } from "@chakra-ui/react";
 import { Link, Outlet } from "react-router-dom";
 import { RiChatNewLine } from "react-icons/ri";
 import "../.././css/Messages.css";
+import { BASE_URL } from "../../utilities/ConstList";
+import { useState, useEffect } from "react";
+
+async function fetchRecentDms(headers) {
+  try {
+    const data = await fetch(`${BASE_URL}/users/recent`, {
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+    });
+
+    const res = await data.json();
+    if (res.errors) {
+      throw res.errors.full_messages[0];
+    } else {
+      return res;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default function Messages() {
+  const headers = JSON.parse(localStorage.getItem("headers") || "{}");
+  const [recentUsers, setRecentUsers] = useState([]);
+
+  async function getRecentDms() {
+    const data = await fetchRecentDms(headers);
+    console.log(data);
+    setRecentUsers(data.data);
+  }
+
+  useEffect(() => {
+    if (headers) {
+      getRecentDms();
+    }
+  }, [headers]);
+
   return (
     <>
       <div className="messages-container">
@@ -21,9 +58,11 @@ export default function Messages() {
           ></Input>
           <h1>Display messages here</h1>
           <div className="conversation-list">
-            <Link to="1">Vince</Link>
-            <Link to="2">Angelo</Link>
-            <Link to="3">Lindsay</Link>
+            {recentUsers.map((user) => (
+              <Link key={user.id} to={`${user.id}`}>
+                {user.email}
+              </Link>
+            ))}
           </div>
         </div>
 
