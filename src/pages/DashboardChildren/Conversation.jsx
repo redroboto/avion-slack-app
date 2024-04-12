aeb0fa202d8be401100b27ef3a25b9e36c897d3a
redroboto <vince.pleyto@gmail.com>
2024-04-12 20:55:34 +0800

    add the send message function to Messages.jsx

--- src/pages/DashboardChildren/Conversation.jsx
+++ src/pages/DashboardChildren/Conversation.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { BASE_URL } from "../../utilities/ConstList";
 import { useState, useEffect } from "react";
+import { Textarea, Button, FormLabel } from "@chakra-ui/react";
+import { LuSendHorizonal } from "react-icons/lu";
 
 async function fetchRecentConversation(headers, user_id) {
   try {
@@ -26,6 +28,7 @@ export default function Conversation() {
   const params = useParams();
   const headers = JSON.parse(localStorage.getItem("headers") || "{}");
   const [conversation, setConversation] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
 
   async function getRecentConversation() {
     const data = await fetchRecentConversation(headers, params.user_id);
@@ -39,13 +42,52 @@ export default function Conversation() {
     }
   }, [params.user_id]);
 
+  async function sendMessage(headers, body) {
+    try {
+      const data = await fetch(`${BASE_URL}/messages`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          ...headers,
+        },
+        body: JSON.stringify(body),
+      });
+      const res = await data.json();
+      console.log(res);
+    } catch (error) {}
+  }
+
+  const handleMessageChange = (e) => setNewMessage(e.target.value);
+
+  async function handleSendMessage(e) {
+    e.preventDefault();
+    const data = await sendMessage(headers, {
+      receiver_id: params.user_id,
+      receiver_class: "User",
+      body: newMessage,
+    });
+    setNewMessage("");
+    getRecentConversation();
+  }
+
   return (
     <>
-      <p>This is {params.user_id}</p>
       <div>
         {conversation.map((message) => {
           return <p key={message.id}>{message.body}</p>;
         })}
+      </div>
+      <div>
+        <form onSubmit={handleSendMessage}>
+          <Textarea
+            type="text"
+            onChange={handleMessageChange}
+            value={newMessage}
+          ></Textarea>
+          <Button type="submit">
+            <LuSendHorizonal />
+          </Button>
+        </form>
       </div>
     </>
   );
